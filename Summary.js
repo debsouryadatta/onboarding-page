6e333cb35cbce2afa162ec9b7fbe2182672a7e4d
debsouryadatta <debsouryadatta@gmail.com>
2023-04-29 10:57:02 +0530

    Initial commit

---
+++ Summary.js
@@ -0,0 +1,7 @@
+// * 1. Create react with vite
+// * 2. Install tailwind css on react + vite
+// * 3. Using tailwind onboarding components and sign in components
+// * 4. Customising them as per preference
+// * 5. Using dark mode in tailwind css
+// * 6. Using useEffect hook to check for the system settings then and also adding the dark class to the index.html as per required
+// * 7. Installing react icons for dark mode icons - Not required for onboarding page
